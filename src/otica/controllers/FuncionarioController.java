7c29969f3e159812afdeb88006cf37a42ac0ad26
unknown <yan05012005@gmail.com>
2025-06-21 04:40:09 -0300

    Controllers testados

--- src/otica/controllers/FuncionarioController.java
+++ src/otica/controllers/FuncionarioController.java
@@ -1,5 +1,31 @@
 package otica.controllers;
 
+import java.util.List;
+import otica.dao.FuncionarioDAO;
+import otica.model.Funcionario;
+
 public class FuncionarioController {
+    
+    private FuncionarioDAO funcionarioDAO;
+
+    public FuncionarioController() {
+        funcionarioDAO = new FuncionarioDAO();
+    }
+
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarioDAO.save(funcionario);
+    }
+
+    public void atualizarFuncionario(Funcionario funcionario) {
+        funcionarioDAO.update(funcionario);
+    }
+
+    public void deletarFuncionario(int funcionarioID) {
+        funcionarioDAO.delete(funcionarioID);
+    }
+
+    public List<Funcionario> listarFuncionarios() {
+        return funcionarioDAO.getFuncionarios();
+    }
 
 }
