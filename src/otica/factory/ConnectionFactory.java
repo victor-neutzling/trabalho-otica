487964b87b80209534e710b630d77c3775b00a18
victor neutzling <victorneutzling2001@gmail.com>
2025-06-22 14:39:34 -0300

    delete main and fix connectionFactory

--- src/otica/factory/ConnectionFactory.java
+++ src/otica/factory/ConnectionFactory.java
@@ -6,12 +6,12 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
 	
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "050105";
+	private static final String PASSWORD = "root";
 	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/otica";
 	
 	public static Connection createConnectionToMySQL() throws Exception {
 		
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/otica", "root", "050105");
+		Connection connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
 		
 		return connection;
 	}
