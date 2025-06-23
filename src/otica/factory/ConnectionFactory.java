25f4f61c094d4337fc7679d1ade78cc45aa5d95f
unknown <yan05012005@gmail.com>
2025-06-23 15:38:01 -0300

    Telefone atualizado para 15

--- src/otica/factory/ConnectionFactory.java
+++ src/otica/factory/ConnectionFactory.java
@@ -6,10 +6,10 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
 	
 	private static final String USERNAME = "root";
-	private static final String PASSWORD1 = "050105";
+	private static final String PASSWORD = "root";
 	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/otica";
-	private static final String PASSWORD2 = "CfWUcsCXPKzgouqfiAlIrdzwkCMjouLK";
-	private static final String DATABASE_URL2 = "jdbc:mysql://nozomi.proxy.rlwy.net:16806/otica";
+	private static final String PASSWORD = "CfWUcsCXPKzgouqfiAlIrdzwkCMjouLK";
+	private static final String DATABASE_URL = "jdbc:mysql://nozomi.proxy.rlwy.net:16806/otica";
 
 	
 	public static Connection createConnectionToMySQL() throws Exception {
